a933274b65c2569c9a9ba06fdfe9dce772332e99
omigusty <omistyfani@gmail.com>
2022-12-29 12:59:26 +0700

    create login and signup page

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask("__main__")
+
+app.config['SECRET_KEY'] = '1ba8a99cbd9984c74cf378e1cdd9f4c4'
 
 posts = [
     {
@@ -18,15 +21,36 @@ posts = [
 ]
 
 
-@app.route("/")
 @app.route("/home")
-def homePage():
+@app.route("/")
+def home():
     return render_template("index.html", posts=posts, title="Home")
 
 
 @app.route("/about")
-def contactPage():
+def about():
     return render_template("about.html", title="About")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "rahasia":
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for("home"))
+        else:
+            flash(f'Login unsuccessful, please check username and password', 'danger')
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
